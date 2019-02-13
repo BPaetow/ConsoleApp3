bb8c2298b9296ebb14c7ff92c441fbdb80af7e4a
BPaetow <bernhardpaetow@gmail.co>
2019-02-12 19:20:43 -0600

    Implemented Basic Login

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,7 +6,23 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int password = 123;
+            string userId = "BPaetow";
+
+            Console.WriteLine("Please enter your user Id:");
+            string userIdInput = Console.ReadLine();
+
+            Console.WriteLine("Please enter your password:");
+            int passwordInput = int.Parse(Console.ReadLine());
+
+            if (userIdInput == userId && passwordInput == password)
+            {
+                Console.WriteLine("Logging in...") ;
+            }
+            else
+            {
+                Console.WriteLine("incorrect ID or Pin!");
+            }
         }
     }
 }
